da3b9b17e40239312f617288f2a6128431bad2da
fabixdev <123942922+fabixdev@users.noreply.github.com>
2024-11-29 10:47:02 +0100

    4

---
+++ src/index-js.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SpotifyController from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SpotifyController />
+  </React.StrictMode>
+);
